ba197de629ac80ded7b0c7d48be5b79b03084e28
TheoCaesar <theocaesar7@gmail.com>
2024-08-01 07:24:58 +0100

    Form Definition & template

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [ReactiveFormsModule, ]
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  onSubmit(){
+    console.log(this.loginForm.value.email, this.loginForm.value.password)
+  }
+}
