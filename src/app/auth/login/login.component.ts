eb226123938d0aeed17642015ba514caccf134ad
TheoCaesar <theocaesar7@gmail.com>
2024-08-01 07:43:30 +0100

    Reactive Forms Validators

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +10,23 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
+    email: new FormControl('', {
+      validators:[Validators.required, Validators.email]
+    }),
+    password: new FormControl('', {
+      validators:[Validators.minLength(8), Validators.required]
+    })
   })
+
+  get emailInvalid(){
+    return this.loginForm.controls.email.touched  && this.loginForm.controls.email.dirty 
+    && this.loginForm.controls.email.invalid 
+  }
+
+  get passwordInvalid(){
+    return this.loginForm.controls.password.touched  && this.loginForm.controls.password.dirty 
+    && this.loginForm.controls.password.invalid 
+  }
 
   onSubmit(){
     console.log(this.loginForm.value.email, this.loginForm.value.password)
