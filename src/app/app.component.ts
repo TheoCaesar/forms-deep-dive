105d057906981dc92f42030883a3862dbe3d6a4e
TheoCaesar <theocaesar7@gmail.com>
2024-08-01 20:16:51 +0100

    half baked solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,6 @@ import { SignupComponent } from "./auth/signup/signup.component";
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent, SignupComponent],
+  imports: [LoginComponent, SignupComponent,],
 })
 export class AppComponent {}
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,9 +1,35 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
+function mustIncludeSymbols(control: AbstractControl){
+  let symbols = ['/', '-', '$', '!', '#', '%', '^', '&', '*']
+  for (let i = 0 ; i< symbols.length ; i++)
+    if (control.value.includes(symbols[i])){ 
+      return null
+    }
+  return {mustIncludeSymbols: true}
+}
 @Component({
   selector: 'app-signup',
   standalone: true,
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
+  imports: [ReactiveFormsModule]
 })
-export class SignupComponent {}
+export class SignupComponent {
+  signupForm = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email]
+    }), 
+    password: new FormControl('', {
+      validators: [Validators.minLength(8), Validators.required, 
+        mustIncludeSymbols]
+    })
+  })
+  
+  onSubmit(){
+    console.log(this.signupForm.value.email, '\n',
+       this.signupForm.value.password)
+    console.dir(this.signupForm)
+  }
+}
